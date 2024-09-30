0fdb9fab66d2ef609ef4ee83bce30dcf5714fc37
Matt Morgan <mattmorgan@Matts-MacBook-Pro.local>
2024-09-30 13:20:55 -0400

    saving

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const num1 = parseInt(process.argv[2]); // parseInt pulls the number '5' out of the string entered ('5' was entered into the terminal as an argument)
-const num2 = +process.argv[3];
+function testOne() {
 
-// const num1 = +process.argv[2]; // the +process is a shorthand for the parseInt above
-
-function sum(a, b) {
-    console.log(a + b);
 }
 
-sum(num1, num2);
+testOne():
+
+const testTwo = () => {
+    console.log('test two');
+}
+
+testTwo():

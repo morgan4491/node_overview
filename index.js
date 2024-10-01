a51a69dc581cc02f2b0a3bb8e5867ab093606ef2
morgan4491 <m.morgan4491@gmail.com>
2024-10-01 13:53:31 -0400

    finished rest/spread operator basics

--- index.js
+++ index.js
@@ -1,154 +1,31 @@
-const nums = [10, 3, 100, 70, 8, 55, 88];
-const users = [
-    {
-        name: 'Bob',
-        age: 99,
-    },
-    {
-        name: 'Jim',
-        age: 25,
-    },
-    {
-        name: 'Sarah',
-        age: 40,
+const fruits = ['orange', 'apple', 'grape'];
+const fruits2 = ['kiwi', 'banana', 'strawberry'];
+const fruits3 = ['pear', 'peach', 'watermelon'];
+
+const combinedArray = [...fruits, 'blueberry', ...fruits2, ...fruits3];  // the '...' is the spread operator - similar to concat, but with more ability to manipulate
+
+
+const data = {
+    name: 'JD',
+    age: 44
+};
+
+
+const combinedData = {
+    ...data,
+    address: '555 coding dr',
+    phone: '777-777-7777'
+};
+
+// console.log(combinedData);
+
+
+
+function printNames (...names) {        // in this instance the '...' is a rest operator that takes your arguments from printNames call, and puts them in an array, and the console.log prints out the array
+    for (const name of names) {
+        console.log(name);
     }
-];
+}
 
-const students = [
-    {
-        name: 'Roman',
-        average: 99
-    },
-    {
-        name: 'Matt',
-        average: 80
-    },
-    {
-        name: 'Izzy',
-        average: 95
-    },
-    {
-        name: 'Kandyce',
-        average: 90
-    }
-]
-
-
-const overallAverage = students.reduce((output, studentObj, index, arr) => {
-    // If we are on the last student, return the output divided by the arr length
-    if (index === arr.length - 1) {
-        return (output + studentObj.average) / arr.length;
-    }
-
-    // Otherwise, return output + the studentObj's average
-    return output + studentObj.average;
-}, 0);
-
-// console.log(overallAverage);
-
-const studentInfo = students.reduce((output, studentObj, index, arr) => {
-    output.overallAverage += studentObj.average;
-    
-    if (index === arr.length - 1) {
-        output.overallAverage = output.overallAverage / arr.length;
-    }
-
-    output.studentCount = arr.length;
-
-    return output;
-
-}, {
-    overallAverage: 0,
-    studentCount: 0
-});
-
-
-
-
-console.log(studentInfo);
-
-
-
-
-
-const sum = nums.reduce((output, num) => {
-    return output + num;
-}, 0);   // the '0' here sets the initial value of output
-
-// console.log(sum);
-
-
-
-
-
-
-
-
-
-
-const evenArray = nums.map((num) => {
-    if ((num % 2) === 0) {
-        return `${num} is even`
-    }   else {
-        return `${num} is odd`
-    }
-    // return num % 2;                         // the '%' symbol gives the remainder of 'num % 2' or the remainder of 'num / 2'
-});
-
-// console.log(evenArray);
-
-
-
-
-
-// nums.forEach((num, index) => {        // forEach is a 'higher order function'
-//     console.log(index);
-// });
-
-// for (const num of nums) {       // this is still useful when you want to stop looping through an array
-//     if(num > 10) {
-//         break;
-//     }
-
-//     console.log(num);
-// };
-
-
-
-
-const filteredNums = nums.filter((num) => {
-    if (num > 10) {
-        return true;
-    }
-});
-
-const filteredUsers = users.filter((userObj) => {
-    if (userObj.age > 30) {
-        return true;
-    }
-});
-
-// console.log(filteredNums);
-
-// console.log(filteredUsers);
-
-
-
-
-
-// Algorithm challeng - write a function that orders the array from lowest to highest
-const sorted = nums.sort((currentNum, nextNum) => {
-    return currentNum - nextNum;
-});
-
-// Create a variable that stores the sorted userArray. In your callback function, return the difference between the current object's age and the next object's age
-
-const userArray = users.sort((currentObj, nextObj) => {
-    return currentObj.age - nextObj.age;
-});
-
-// Create a variable that stores the sorted users array and sorts the objects by name - hint (all letter characters have a number that you can pull with JS)
-
-// console.log(userArray);
-
-// console.log(sorted);
+printNames('Bob', 'Jane', 'Frank', 'Jill', 'Billy');
+printNames('Jake', 'Bill');

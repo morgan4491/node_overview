01208d60707b0c47f4a4adc67a83a69f70623281
morgan4491 <m.morgan4491@gmail.com>
2024-10-01 14:35:22 -0400

    started readfiles

--- index.js
+++ index.js
@@ -1,34 +1,9 @@
-const data = {
-    name: 'JD',
-    age: 44,
-    address: '555 coding dr',
-    phone: '777-777-7777',
-    info: {
-        location: 'Atlanta',
-        county: 'Cobb',
-        places: ['Lake Acworth', 'Georgia Aquarium']
+const fs = require('fs');
+
+fs.readFile('./names.txt', 'utf8', (error, data) => {
+    if(error) {
+        return console.log(error);
     }
-};
 
-const {age, address, info: {location}} = data;         // Within the {}, write the properties you want to pull from the object that comes after the '=' sign
-
-console.log(location);
-
-
-
-
-function printData(obj) {
-    console.log(obj);
-};
-
-printData({
-    color: 'blue',
-    length: 10
-})
-
-
-const fruits = ['orange', 'apple', 'kiwi'];
-
-const [a, b, thatFruit] = fruits;
-
-console.log(thatFruit);
+    console.log(data);
+});

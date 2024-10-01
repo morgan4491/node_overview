03ebc7aa0a391d30794726158eba66a039743898
morgan4491 <m.morgan4491@gmail.com>
2024-10-01 14:21:32 -0400

    finished destructuring basics

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const fruits = ['orange', 'apple', 'grape'];
-const fruits2 = ['kiwi', 'banana', 'strawberry'];
-const fruits3 = ['pear', 'peach', 'watermelon'];
-
-const combinedArray = [...fruits, 'blueberry', ...fruits2, ...fruits3];  // the '...' is the spread operator - similar to concat, but with more ability to manipulate
-
-
 const data = {
     name: 'JD',
-    age: 44
-};
-
-
-const combinedData = {
-    ...data,
+    age: 44,
     address: '555 coding dr',
-    phone: '777-777-7777'
+    phone: '777-777-7777',
+    info: {
+        location: 'Atlanta',
+        county: 'Cobb',
+        places: ['Lake Acworth', 'Georgia Aquarium']
+    }
 };
 
-// console.log(combinedData);
+const {age, address, info: {location}} = data;         // Within the {}, write the properties you want to pull from the object that comes after the '=' sign
+
+console.log(location);
 
 
 
-function printNames (...names) {        // in this instance the '...' is a rest operator that takes your arguments from printNames call, and puts them in an array, and the console.log prints out the array
-    for (const name of names) {
-        console.log(name);
-    }
-}
 
-printNames('Bob', 'Jane', 'Frank', 'Jill', 'Billy');
-printNames('Jake', 'Bill');
+function printData(obj) {
+    console.log(obj);
+};
+
+printData({
+    color: 'blue',
+    length: 10
+})
+
+
+const fruits = ['orange', 'apple', 'kiwi'];
+
+const [a, b, thatFruit] = fruits;
+
+console.log(thatFruit);
